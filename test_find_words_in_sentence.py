b286610654eb90a166b824ac6c1bf825018b4dfc
Nagaraj Sundaramahalingam <nagaraj.sundaramahalingam@hpe.com>
2021-10-06 12:10:54 +0530

    cleanup code

--- test_find_words_in_sentence.py
+++ test_find_words_in_sentence.py
@@ -12,23 +12,22 @@ import unittest
 from word_finder import search_in_sequence
 
 
-class WordSequence_test(unittest.TestCase):
+class TestWordSequence(unittest.TestCase):
 
     def test_word_in_sequence(self):
+
         word_sequence = "Today is toDay and the day is 4"
         word_sequence_1 = "12 2 f r t ty 12 2"
-        search_word_4 = "12 2"
-        search_word_1 = "todaY"
-        search_word_2 = "from"
-        output_1 = search_in_sequence(word_sequence, search_word_1)
-        self.assertEqual(output_1, ("Yes", 2))
-        output_2 = search_in_sequence(word_sequence, search_word_2)
-        self.assertEqual(output_2, None)
-        search_word_3 = "4"
-        output_3 = search_in_sequence(word_sequence, search_word_3)
-        self.assertEqual(output_3, ("Yes", 1))
-        output_4 = search_in_sequence(word_sequence_1, search_word_4)
-        self.assertEqual(output_4, ("Yes", 2))
+
+        self.assertEqual(search_in_sequence(
+            word_sequence, "todaY"), ("Yes", 2))
+
+        self.assertEqual(search_in_sequence(word_sequence, "from"), None)
+
+        self.assertEqual(search_in_sequence(word_sequence, "4"), ("Yes", 1))
+
+        self.assertEqual(search_in_sequence(
+            word_sequence_1, "12 2"), ("Yes", 2))
 
 
 if __name__ == "__main__":
